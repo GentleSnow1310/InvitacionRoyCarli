9fb38efd58f926e4dda90b56b38cb5591d0ee6c4
GentleSnow1310 <pallettgaraybenjamin@gmail.com>
2025-06-27 19:20:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.addEventListener('load', () => { const intro = document.getElementById('i
 
 // Mostrar burbujas después de pareja setTimeout(() => { burbujas.classList.remove('oculto'); }, 6000); });
 
+-
 function confirmarAsistencia(asiste) {
   if (asiste) {
     alert("¡Gracias por confirmar tu asistencia! 💚");
