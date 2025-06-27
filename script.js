b6a18c36ddab91b96fbfe27ca8607158ba7242f0
GentleSnow1310 <pallettgaraybenjamin@gmail.com>
2025-06-27 18:55:00 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// scripts.js
+
+window.addEventListener('load', () => { const intro = document.getElementById('intro'); const pareja = document.getElementById('pareja'); const burbujas = document.getElementById('burbujas');
+
+// Mostrar pareja después de intro setTimeout(() => { intro.style.display = 'none'; pareja.classList.remove('oculto'); }, 3000);
+
+// Mostrar burbujas después de pareja setTimeout(() => { burbujas.classList.remove('oculto'); }, 6000); });
+
+
 function confirmarAsistencia(asiste) {
   if (asiste) {
     alert("¡Gracias por confirmar tu asistencia! 💚");
